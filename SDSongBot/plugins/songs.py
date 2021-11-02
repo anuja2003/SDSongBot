2dd43a39b8866b9c311c472413126778fe66cffd
Anuja Supulsara <88157026+anuja2003@users.noreply.github.com>
2021-11-02 21:47:36 +0530

    Update songs.py

--- SDSongBot/plugins/songs.py
+++ SDSongBot/plugins/songs.py
@@ -26,7 +26,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 Finding the song...')
+    m = message.reply('🔎 Finding The Your😊❤️ Song...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -48,13 +48,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("📥 Downloading the song by @SDbotsz 📥")
+    m.edit("**හරි ඔයා හොයපු Song එක මට හම්බ වුණෝ 😃📥** \n\n ılı _.●- UPLOADING-●._ılı \n ▄ █ ▄ █ ▄ ▄ █ ▄ █ ▄ █ \n Min- – – – – – – – – -●Max\n\n**🎯 Percentage : 100.0%**\n**💫 Done: ✅**\n**⚙️ Total: 🔮 SONG●●●**\n**🚀 Speed: Fast🎲**\n**🕒 ETA: 3s📤😄**")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**~ Uploaded by @SDbotsz ~**'
+        rep = '**🎯 Uploaded by @sinhalasin 📥❤️**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -62,7 +62,7 @@ def song(client, message):
         s = message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit('❌ Error')
+        m.edit('❌ Error😢')
         print(e)
 
     try:
