a55cabe5e57564ff49e3399995d5e9db00c856e5
Anuja Supulsara <88157026+anuja2003@users.noreply.github.com>
2021-11-02 22:03:33 +0530

    Update songs.py

--- SDSongBot/plugins/songs.py
+++ SDSongBot/plugins/songs.py
@@ -54,7 +54,8 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎯 Uploaded by @sinhalasin 📥❤️**'
+        rep = '**🎯 Uploaded by @sinhalasin 📥❤️** \n **Join🙈❤️ \n My Channel  👉 @sinhalasin 😍**'
+      '
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
